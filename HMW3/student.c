cc3a5fc1920a63831f3f0b3528f05ffc5b4190e1
Gadishenhar <gadi.shenhar@gmail.com>
2020-06-05 12:35:07 +0300

    Student +CountPreProc

--- HMW3/student.c
+++ HMW3/student.c
@@ -18,9 +18,11 @@ PStudent StudentCreate(char* name, int age, int ID, char* faculty) {
 	NewStudent = (PStudent)malloc(sizeof(Student));
 	if (NewStudent == NULL)
 		return NULL;
+	NewStudent->name = (char*)malloc(sizeof(strlen(name))+1);
 	strcpy(NewStudent->name, name);
 	NewStudent->age = age;
 	NewStudent->ID = ID;
+	NewStudent->faculty = (char*)malloc(sizeof(strlen(faculty))+1);
 	strcpy(NewStudent->faculty, faculty);
 
 	return NewStudent;
@@ -28,7 +30,7 @@ PStudent StudentCreate(char* name, int age, int ID, char* faculty) {
 void printStudent(PStudent Student) {
 	if (Student == NULL)
 		return;
-	printf("Name: %s, Age: %d, ID: %d, faculty %s\n", Student->name, Student->age, Student->ID, Student->faculty);
+	printf("Name: %s, Age: %d, ID: %d, Faculty: %s\n", Student->name, Student->age, Student->ID, Student->faculty);
 }
 
 PStudent cloneStudent(PStudent Student) {
@@ -38,36 +40,36 @@ PStudent cloneStudent(PStudent Student) {
 	NewStudent = (PStudent)malloc(sizeof(Student));
 	if (NewStudent == NULL)
 		return NULL;
+	NewStudent->name = (char*)malloc(sizeof(strlen(Student->name))+1);
 	strcpy(NewStudent->name, Student->name);
 	NewStudent->age = Student->age;
 	NewStudent->ID = Student->ID;
+	NewStudent->faculty = (char*)malloc(sizeof(strlen(Student->faculty))+1);
 	strcpy(NewStudent->faculty, Student->faculty);
 	
 	return NewStudent;
 }
 
 void destroyStudent(PStudent Student) {
-	if (Student == NULL)
+	if (Student == NULL) 
 		return;
+	free(Student->name);
+	free(Student->faculty);
 	free(Student);
 }
 
 BOOL compareStudents(PStudent Student1, PStudent Student2) {
-
-	if (Student1 == NULL && Student2 == NULL)
-		return TRUE; 
-	else if (Student1 == NULL)
-		return FALSE;
-	else if (Student2== NULL)
+ 
+	if (Student1 == NULL || Student2 == NULL)
 		return FALSE;
 
-	if (Student1->name != Student2->name)
+	if (!strcmp(Student1->name,Student2->name))
 		return FALSE;
 	if (Student1->age != Student2->age)
 		return FALSE;
 	if (Student1->ID != Student2->ID)
 		return FALSE;
-	if (Student1->faculty != Student2->faculty)
+	if (!strcmp(Student1->faculty,Student2->faculty))
 		return FALSE;
 
 	return TRUE;
