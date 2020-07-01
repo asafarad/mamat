d4d861975e5ec4dde02179de7f1adf2e1117ea34
asafarad <asaf.arad@campus.technion.ac.il>
2020-07-01 19:12:09 +0300

    added tar file

--- HMW5/compfunc.h
+++ HMW5/compfunc.h
@@ -3,11 +3,6 @@
 #define _COMP_F
 #include "func.h"
 
-/**
-* @ brief A class which represent a complex function and 
-* holds pointers to an inner function and an outer function
-*/
-
 class compfunc : public func {
 	
 	public:
